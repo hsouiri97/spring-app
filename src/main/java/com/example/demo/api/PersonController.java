453345ef0ca2f253a6c926dc61be585e0a371273
hsouiri97 <hamza.souiri97@gmail.com>
2020-02-29 21:27:18 +0100

    added-crud-functionality

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
 
-
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
@@ -25,5 +26,23 @@ public class PersonController {
     @GetMapping
     public List<Person> getAllPeople() {
         return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id).orElse(null);
+        //Return a 404 Status in a typical situation instead of null
+    }
+
+    @PutMapping(path = "{id}")
+    public Person updatePersonById(@PathVariable("id") UUID id, @RequestBody Person person) {
+        if(personService.updatePerson(id, person) == 1)
+            return getPersonById(id);
+        return null;
+    }
+
+    @DeleteMapping(path = "{id}")
+    public int deletePersonById(@PathVariable("id") UUID id) {
+        return personService.deletePersonById(id);
     }
 }
